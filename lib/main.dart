9f96cae92be98fb4b9e7b8884387a554196a3c15
hydev777 <hytechwilson@gmail.com>
2022-01-19 11:22:35 -0400

    init

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/category_meals_screen.dart';
+import 'screens/categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        canvasColor: Colors.greenAccent,
+        colorScheme: ColorScheme.fromSwatch(accentColor: Colors.grey),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light()
+            .textTheme
+            .copyWith(bodyText1: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)), bodyText2: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1))),
+      ),
+      // home: const CategoriesScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (ctx) => const CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => const CategoryMealsScreen()
+      },
+    );
+  }
+}
---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:meals/dummy_dart.dart';
+import 'package:meals/widgets/meal_item.dart';
+// import '../models/meal.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  const CategoryMealsScreen({Key? key}) : super(key: key);
+
+  static const routeName = '/category-meals';
+
+  @override
+  Widget build(BuildContext context) {
+
+    final routeArgs = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories!.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar( title: Text(categoryTitle!) ),
+      body: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (ctx, i) {
+            return MealItem(title: categoryMeals[i].title, imageUrl: categoryMeals[i].imageUrl, duration: categoryMeals[i].duration, complexity: categoryMeals[i].complexity, affordability: categoryMeals[i].affordability);
+          },
+      ),
+    );
+  }
+}
